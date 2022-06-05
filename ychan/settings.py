abf6a5c72b0cf215761aeb80004385fa2363244b
nohakai <90017985+nohakai@users.noreply.github.com>
2022-06-05 21:08:13 +0900

    Update settings.py

--- ychan/settings.py
+++ ychan/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fis@=7)fk&jg)%dm$uf!6sh(2w$w%gxkkt-@i89oi%c*998as('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
